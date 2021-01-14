55e343b3ee4990644cad48349d392f2b7b0a21fc
Jeff <jeffli.1644@gmail.com>
2021-01-13 19:23:48 -0800

    font changes

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "JetBrains Mono Nerd Font:size=1:antialias=true:autohint=true";
 static char *font2[] = {
 	"Nerd Fonts:style=Regular"
 };
